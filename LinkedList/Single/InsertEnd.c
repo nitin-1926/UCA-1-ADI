0950f868cc7b588c42de43c601d395951b2a5f1d
Nitin Gupta <gupta7nitin@gmail.com>
2019-10-06 13:48:57 +0530

    :)

---
+++ LinkedList/Single/InsertEnd.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node{
+    int data;
+    struct Node *next;
+};
+struct Node* head = NULL;
+
+void print(struct Node* head){
+    if(head==NULL){
+        printf("Linked List Empty");
+    }
+    struct Node* ptr = head;
+    while(ptr){
+        printf("%d -> ",ptr->data);
+        ptr = ptr->next;
+    }
+}
+
+void insertEnd(struct Node** head,int num){
+    struct Node* node = (struct Node *)malloc(sizeof(struct Node));
+    node->data = num;
+    node->next = NULL;
+    if(*head==NULL){
+        *head = node;
+        return;
+    }
+    struct Node* ptr = *head;
+    while(ptr->next){
+        ptr = ptr->next;
+    }
+    ptr->next = node;
+}
+
+int main(){
+    printf("Enter the number of elements in the Linked List\n");
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        int num;
+        scanf("%d",&num);
+        insertEnd(&head,num);
+    }
+    print(head);
+    return 0;
+}
