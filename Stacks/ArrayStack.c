0c87d37c794505d698bc4b7f360bed1c2ba800aa
Nitin Gupta <gupta7nitin@gmail.com>
2019-10-13 13:56:15 +0530

    :)

---
+++ Stacks/ArrayStack.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+
+#define SIZE 20
+
+int Stack[SIZE], top=-1;
+
+int isFull()
+{
+  return top==(SIZE-1);
+}
+
+int isEmpty()
+{
+  return top==-1;
+}
+
+int push(int item)
+{
+  if (isFull())
+  {
+    return -1;
+  }
+  Stack[++top] = item;
+}
+
+int pop()
+{
+  int temp;
+  if (isEmpty())
+  {
+    return -1;
+  }
+  temp=Stack[top--];
+
+  return temp;
+}
+
+int main()
+{
+  int temp;
+  push(56);
+  push(544);
+  temp=pop();
+  printf("%d\n",temp);
+  push(8);
+  temp=pop();
+  printf("%d\n",temp);
+  temp=pop();
+  printf("%d\n",temp);
+  temp=pop();
+  printf("%d\n",temp);
+  return 0;
+}
