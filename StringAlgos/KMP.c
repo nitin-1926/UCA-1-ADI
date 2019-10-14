851ad8e4cef2d0b4d40bfeb2357298dc5f30f4c8
Nitin Gupta <gupta7nitin@gmail.com>
2019-10-14 19:43:38 +0530

    :)

--- StringAlgos/KMP.c
+++ StringAlgos/KMP.c
@@ -8,16 +8,12 @@ void KMPSearch(char *pat, char *txt)
 {
     int M = strlen(pat);
     int N = strlen(txt);
- 
-    // create lps[] that will hold the longest prefix suffix
-    // values for pattern
     int *lps = (int *)malloc(sizeof(int)*M);
-    int j  = 0;  // index for pat[]
+    int j  = 0;
  
-    // Preprocess the pattern (calculate lps[] array)
     computeLPSArray(pat, M, lps);
  
-    int i = 0;  // index for txt[]
+    int i = 0;
     while (i < N)
     {
       if (pat[j] == txt[i])
@@ -32,29 +28,25 @@ void KMPSearch(char *pat, char *txt)
         j = lps[j-1];
       }
  
-      // mismatch after j matches
       else if (i < N && pat[j] != txt[i])
       {
-        // Do not match lps[0..lps[j-1]] characters,
-        // they will match anyway
         if (j != 0)
          j = lps[j-1];
         else
          i = i+1;
       }
     }
-    free(lps); // to avoid memory leak
+    free(lps);
 }
  
 void computeLPSArray(char *pat, int M, int *lps)
 {
-    int len = 0;  // length of the previous longest prefix suffix
+    int len = 0;
     int i;
  
-    lps[0] = 0; // lps[0] is always 0
+    lps[0] = 0;
     i = 1;
  
-    // the loop calculates lps[i] for i = 1 to M-1
     while (i < M)
     {
        if (pat[i] == pat[len])
@@ -67,13 +59,9 @@ void computeLPSArray(char *pat, int M, int *lps)
        {
          if (len != 0)
          {
-           // This is tricky. Consider the example 
-           // AAACAAAA and i = 7.
            len = lps[len-1];
- 
-           // Also, note that we do not increment i here
-         }
-         else // if (len == 0)
+          }
+         else
          {
            lps[i] = 0;
            i++;
@@ -81,8 +69,6 @@ void computeLPSArray(char *pat, int M, int *lps)
        }
     }
 }
- 
-// Driver program to test above function
 int main()
 {
    char *txt = "ABABDABACDABABCABAB";
