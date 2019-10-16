573c47d63de59d2803d6336bd788f069d302469f
Nitin Gupta <gupta7nitin@gmail.com>
2019-10-16 18:45:33 +0530

    :)

--- BinarySearch/PairsWithSumK.c
+++ BinarySearch/PairsWithSumK.c
@@ -82,6 +82,7 @@ int main(){
     int n;
     scanf("%d",&n);
     int arr[n];
+    printf("Enter Elements of the Array in sorted order\n");
     for(int i=0;i<n;i++)
         scanf("%d",&arr[i]);
     int sum;
