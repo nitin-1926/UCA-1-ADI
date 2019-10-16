8a74759478d0fc5c86c9d4520ffd114cb1c55636
Nitin Gupta <gupta7nitin@gmail.com>
2019-10-16 16:09:06 +0530

    :)

--- BinarySearch/PairsWithSumK.c
+++ BinarySearch/PairsWithSumK.c
@@ -1,4 +1,56 @@
 #include<stdio.h>
+
+void merge(int arr[], int l, int m, int r){ 
+    int i=0;
+    int j=0;
+    int k=l; 
+    int n1 = m - l + 1; 
+    int n2 =  r - m; 
+  
+    int Left[n1], Right[n2]; 
+
+    for (i = 0; i < n1; i++) 
+        Left[i] = arr[l + i]; 
+    for (j = 0; j < n2; j++) 
+        Right[j] = arr[m + 1+ j]; 
+    i=0;
+    j=0;
+  
+    while (i < n1 && j < n2){ 
+        if (Left[i] <= Right[j]){ 
+            arr[k] = Left[i]; 
+            i++; 
+        } 
+        else{ 
+            arr[k] = Right[j]; 
+            j++; 
+        } 
+        k++; 
+    } 
+    while (i < n1){ 
+        arr[k] = Left[i]; 
+        i++; 
+        k++; 
+    } 
+    while (j < n2){ 
+        arr[k] = Right[j]; 
+        j++; 
+        k++; 
+    } 
+} 
+
+void mergeSort(int arr[], int left, int right){ 
+    if (left < right){ 
+
+        int m = left+(right-left)/2; 
+
+        mergeSort(arr, left, m); 
+        mergeSort(arr, m+1, right); 
+  
+        merge(arr, left, m, right); 
+    } 
+}
+
 int countpair(int ar[],int s,int e,int sum){
     int c=0;
     while(s<e){
@@ -35,5 +87,6 @@ int main(){
     int sum;
     printf("Enter the sum you want to check");
     scanf("%d",&sum);
-    printf("\nNumber of pairs with the given sum are: %d\n",countpair(ar,0,n-1,sum));
+    mergeSort(arr,0,n-1);
+    printf("\nNumber of pairs with the given sum are: %d\n",countpair(arr,0,n-1,sum));
 }
