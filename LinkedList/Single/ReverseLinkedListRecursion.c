6b01473ef5fbbc555f8d35dd0bba9fe8c1d4a523
Nitin Gupta <gupta7nitin@gmail.com>
2019-10-17 01:32:36 +0530

    :)

---
+++ LinkedList/Single/ReverseLinkedListRecursion.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node
+{
+  int value;
+  struct Node *next;
+} Node;
+
+
+Node *head = NULL;
+
+Node *createNode(int v, Node *next)
+{
+  Node *newnode = (Node *)malloc(sizeof(Node *));
+  newnode->value = v;
+  newnode->next = next;
+  return newnode;
+}
+
+void insertEnd(int v,Node **head)
+{
+  Node *newnode = createNode(v, NULL);
+  if (*head == NULL){
+	*head = newnode;
+  }
+  else{
+	Node *ptr = *head;
+	while (ptr->next != NULL){
+	  ptr = ptr->next;
+	}
+	ptr->next = newnode;
+  }
+}
+
+void printList(Node *head)
+{
+  Node *ptr = head;
+  if (head == NULL)
+  {
+	printf("NULL");
+  }
+  //return;
+  while (ptr != NULL)
+  {
+	printf("%d",ptr->value);
+	ptr = ptr->next;
+  }
+  printf("\n");
+}
+
+void insertBeg(int v, Node** head){
+  if(*head==NULL){
+	*head=createNode(v,NULL);
+  }
+  else{
+	Node *np = createNode(v,*head);
+	*head = np;
+  }
+  
+}
+
+void reverseFunc(Node *head,Node** np){
+  if(head->next==NULL){
+    insertEnd(head->value,np);
+  }
+  else{
+    reverseFunc(head->next,np);
+    insertEnd(head->value, np);
+  }
+}
+
+Node *reverseLL(Node *head)
+{
+  Node *newLL =NULL;
+  reverseFunc(head,&newLL);
+  return newLL;
+}
+
+int main()
+{
+  insertEnd(1,&head);
+  insertEnd(2,&head);
+  insertEnd(3,&head);
+  insertEnd(4,&head);
+  insertEnd(5,&head);
+  insertEnd(5,&head);
+  insertBeg(1,&head);
+  printList(head);
+  
+	Node *newLL = reverseLL(head);
+  printList(newLL);
+  
+  return 0;
+}
