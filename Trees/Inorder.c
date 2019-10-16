7e936934eb12bd995d589683dd7a013103f18490
Nitin Gupta <gupta7nitin@gmail.com>
2019-10-16 22:30:14 +0530

    :)

---
+++ Trees/Inorder.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct tree{
+    int data;
+    struct tree *left;
+    struct tree *right;
+};
+void print(struct tree* root){
+    if(root==NULL){
+        return;
+    }
+    print(root->left);
+    printf("%d ",root->data);
+    print(root->right);
+}
+struct tree* makeNode(int item){
+    struct tree *new_node = (struct tree*)malloc(sizeof(struct tree));
+    new_node->data = item;
+    new_node->left = NULL;
+    new_node->right = NULL;
+    return new_node;
+}
+struct tree* insertNode(struct tree* root,int item){
+    if(root==NULL){
+        return makeNode(item);
+    }
+    if(item < root->data){
+        root->left = insertNode(root->left,item);
+    }
+    if(item > root->data){
+        root->right = insertNode(root->right,item);
+    }
+    return root;
+}
+
+int main(){
+    struct tree* root = makeNode(50);
+    insertNode(root,20);
+    insertNode(root,80);
+    insertNode(root,10);
+    insertNode(root,40);
+    insertNode(root,70);
+    insertNode(root,60);
+    insertNode(root,30);
+    insertNode(root,90);
+    print(root);
+    return 0;
+}
