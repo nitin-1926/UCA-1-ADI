48f666c08a6a426a2ba15a2a8bbbd74ef77519af
Nitin Gupta <gupta7nitin@gmail.com>
2019-10-19 23:19:04 +0530

    :)

--- Stacks/RemoveCharacterWith4orMoreRepetitions.cpp
+++ Stacks/RemoveCharacterWith4orMoreRepetitions.cpp
@@ -8,31 +8,35 @@ void remove_duplicates(string str){
         char char_array[n+1];
         int count=0;
         strcpy(char_array,str.c_str());
-	stk.push(char_array[0]);
-        for(int i=1;i<n;i++){
+         stk.push(char_array[0]);
+        for(int i=1;i<=n;i++){
                 if(char_array[i]!=stk.top())
-			{
-		          	if(count>=4)
-				 {
-			                while(count!=0)
-					{
-		                        	stk.pop();
-		                        	count--;
-		        		}
-				stk.pop();
-				}
-			stk.push(char_array[i]);
-			count=0;
-           		}
-                else
-		{
+             {
+                 if(count>=2)
+               {
+               while(count!=0)
+                   {
+                        stk.pop();
+                        count--;
+                    }
+             stk.pop();
+           }
+            stk.push(char_array[i]);
+            count=0;
+            }
+                else {
                         count++;
                         stk.push(char_array[i]);
                 }
         }
+        stack <char> stk1;
         while(stk.empty()==false){
-                cout<<stk.top();
+                stk1.push(stk.top());
                 stk.pop();
+        }
+        while(stk1.empty()==false){
+            cout<<stk1.top();
+            stk1.pop();
         }
 }
 int main(int argc,const char *argv[]){
