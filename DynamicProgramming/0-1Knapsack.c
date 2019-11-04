eea68bd309edad446ed6631cdd2589ddf9bcc800
Nitin Gupta <gupta7nitin@gmail.com>
2019-11-04 19:01:18 +0530

    :)

---
+++ DynamicProgramming/0-1Knapsack.c
@@ -0,0 +1,33 @@
+#include<stdio.h> 
+int max(int a, int b) { return (a > b)? a : b; } 
+
+int knapSack(int W, int wt[], int val[], int n) 
+{ 
+int i, w; 
+int K[n+1][W+1]; 
+
+for (i = 0; i <= n; i++) 
+{ 
+	for (w = 0; w <= W; w++) 
+	{ 
+		if (i==0 || w==0) 
+			K[i][w] = 0; 
+		else if (wt[i-1] <= w) 
+				K[i][w] = max(val[i-1] + K[i-1][w-wt[i-1]], K[i-1][w]); 
+		else
+				K[i][w] = K[i-1][w]; 
+	} 
+} 
+
+return K[n][W]; 
+} 
+
+int main() 
+{ 
+	int val[] = {160, 100, 120}; 
+	int wt[] = {1, 2, 3}; 
+	int W = 5; 
+	int n = sizeof(val)/sizeof(val[0]); 
+	printf("%d\n", knapSack(W, wt, val, n)); 
+	return 0; 
+} 
