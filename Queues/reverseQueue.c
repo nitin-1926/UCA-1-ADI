629644e028a5afae68e0c5772da91fb1fe403fb9
Nitin Gupta <gupta7nitin@gmail.com>
2019-11-25 17:24:53 +0530

    :)

---
+++ Queues/reverseQueue.c
@@ -0,0 +1,17 @@
+/*
+int front=-1, rear=-1;
+int array[SIZE];
+void enqueue(int item);
+int dequeue();
+
+Above variables are used for queue, SIZE, front and rear and all are global variables. Complete the below method. */
+
+void reverseQueue(int q[])
+{
+  if(front>rear){
+    return;
+  }
+  int data = dequeue();
+  reverseQueue(q);
+  enqueue(data);
+}
